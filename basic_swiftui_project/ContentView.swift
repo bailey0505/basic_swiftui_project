3fd727308036a92ee4710bad83c8ddb2986bfbb1
Bailey Rotellini <baileyrotellini1998@gmail.com>
2021-04-26 07:18:33 -0400

    Initial Commit

---
+++ basic_swiftui_project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basic_swiftui_project
+//
+//  Created by Bailey Rotellini on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ basic_swiftui_project/basic_swiftui_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  basic_swiftui_projectApp.swift
+//  basic_swiftui_project
+//
+//  Created by Bailey Rotellini on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct basic_swiftui_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
