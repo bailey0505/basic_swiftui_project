016f9b6c6c82f0f57e1220771b78c8c92ffe5f47
Bailey Rotellini <baileyrotellini1998@gmail.com>
2021-04-26 08:43:16 -0400

    Finishing up part 1

--- basic_swiftui_project/views/navigation.swift
+++ basic_swiftui_project/views/navigation.swift
@@ -25,7 +25,6 @@ struct navigation: View {
         }
     }
 }
-
 struct navigation_Previews: PreviewProvider {
     static var previews: some View {
         navigation()
--- basic_swiftui_project/views/part1.swift
+++ basic_swiftui_project/views/part1.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct part1: View {
+    @State private var selection = 1
+    @State private var paragraph = ""
     var body: some View {
-        Text("Part 1")
+        VStack(spacing: 0) {
+            Text("Reading User input and returning Value")
+            .multilineTextAlignment(.center)
+            .font(.body)
+            .padding()
+            Spacer()
+            
+            Text("Picker One to learn")
+            .font(Font.headline.weight(.bold))
+            
+            Picker(selection: $selection, label: Text("Select An option")) {
+                Text("Wine").tag(1)
+                Text("Beer").tag(2)
+                Text("Whiskey").tag(3)
+                
+            }
+            Button(action: {
+               
+                if(self.selection == 1){
+                    self.paragraph = "Wine is a great beverage"
+                }else if(self.selection == 2){
+                    self.paragraph = "Beer is a more superior beverage to wine"
+                }else{
+                    self.paragraph = "Whiskey is the ultimate beverage"
+                }
+            }){
+                Text("Submit")
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .top, endPoint: .bottom))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .padding(10)
+                    .cornerRadius(40)
+            }
+            
+            
+            Text(self.paragraph)
+        }
+        
+        .offset(y: -50)
+        .frame(minWidth: 0, maxHeight: 400, alignment: .topLeading)
+
     }
 }
+
 
 struct part1_Previews: PreviewProvider {
     static var previews: some View {
         part1()
     }
 }
+
+
